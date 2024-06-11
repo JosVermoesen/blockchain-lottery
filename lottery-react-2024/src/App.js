093026c5ca1d3136c0471d54c65fb985c35b9b35
JosVermoesen <josvermoesen@outlook.be>
2024-06-11 13:43:43 +0200

    learning react client for web3 4.9.0

--- lottery-react-2024/src/App.js
+++ lottery-react-2024/src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import web3 from './web3';
-import lottery from './lottery';
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
   state = {
-    manager: '',
+    manager: "",
     players: [],
-    balance: '',
-    value: '',
-    message: '',
-    
+    balance: "",
+    value: "",
+    message: "",
   };
-
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
@@ -25,40 +23,38 @@ class App extends React.Component {
 
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: 'Waiting on transaction success...' });
+    this.setState({ message: "Waiting on transaction success..." });
 
     await lottery.methods.enter().send({
       from: accounts[0],
-      value: web3.utils.toWei(this.state.value, 'ether'),
+      value: web3.utils.toWei(this.state.value, "ether"),
     });
 
-    this.setState({ message: 'You have been entered!' });
+    this.setState({ message: "You have been entered!" });
   };
 
   onClick = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: 'Waiting on transaction success...' });
+    this.setState({ message: "Waiting on transaction success..." });
 
     await lottery.methods.pickWinner().send({
       from: accounts[0],
     });
 
-    this.setState({ message: 'A winner has been picked!' });
+    this.setState({ message: "A winner has been picked!" });
   };
 
   render() {
-    // console.log(web3.version);
-    // web3.eth.getAccounts().then(console.log);
-
     return (
       <div>
-        <h2>Lottery Contract (React)</h2>
+        <h2>Lottery Contract</h2>
         <p>
-          This contract is managed by {this.state.manager}. There are currently{' '}
-          {this.state.players.length} people entered, competing to win{' '}
-          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
+
         <hr />
         <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
---
+++ lottery-react-2024/src/lottery.js
@@ -0,0 +1,62 @@
+import web3 from "./web3";
+
+// web3 4.9.0 version
+// address coming from the deploy.js file on kickstart bundle project
+// abi coming from the compile4JSON.js build file on kickstart bundle project
+// https://github.com/JosVermoesen/kickstart-bundle
+
+const address = "0x6Eaf66F5fa7db3c0a41108ed8805C3565AF22572";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+export default new web3.eth.Contract(abi, address);
